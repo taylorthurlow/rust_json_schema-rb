b0144bde6cea85ffda9fa547bfdf370a0200ce55
Taylor Thurlow <thurlow@hey.com>
2024-01-11 15:19:21 -0800

    Add basic specs

--- spec/rust_json_schema_spec.rb
+++ spec/rust_json_schema_spec.rb
@@ -1,11 +1,27 @@
 # frozen_string_literal: true
 
+require "json"
+
 RSpec.describe RustJSONSchema do
   it "has a version number" do
     expect(RustJSONSchema::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe RustJSONSchema::Validator do
+    it "raises an error when the input schema is not valid JSON" do
+      schema = "not valid json"
+
+      expect {
+        RustJSONSchema::Validator.new(schema)
+      }.to raise_exception(RustJSONSchema::JSONParseError)
+    end
+
+    it "raises an error when the input schema is valid JSON but not a valid schema" do
+      schema = '{"type": "invalidtype"}'
+
+      expect {
+        RustJSONSchema::Validator.new(schema)
+      }.to raise_exception(RustJSONSchema::SchemaParseError)
+    end
   end
 end
