8c276bc000730166c1042dc49b561868b247c000
Taylor Thurlow <thurlow@hey.com>
2024-01-11 18:44:36 -0800

    Try to fix require-by-ruby-version

--- lib/rust_json_schema.rb
+++ lib/rust_json_schema.rb
@@ -1,7 +1,14 @@
 # frozen_string_literal: true
 
 require_relative "rust_json_schema/version"
-require_relative "rust_json_schema/rust_json_schema"
+
+# Tries to require the extension for the given Ruby version first
+begin
+  RUBY_VERSION =~ /(\d+\.\d+)/
+  require "rust_json_schema/#{Regexp.last_match(1)}/rust_json_schema"
+rescue LoadError
+  require_relative "rust_json_schema/rust_json_schema"
+end
 
 module RustJSONSchema
   class Error < StandardError; end
